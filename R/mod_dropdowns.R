d27daf0d728cac2a7bf5e77d61593c13c1f7f275
shahreyar-abeer <shahreyar.abeer@gmail.com>
2020-10-12 15:21:01 +0600

    module for dropdowns

---
+++ R/mod_dropdowns.R
@@ -0,0 +1,77 @@
+#' dropdowns UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+#' @importFrom shiny.semantic dropdown_input update_dropdown_input
+#' @import data.table
+mod_dropdowns_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    
+    div(class = "ui grid",
+        div(class = "three column row",
+            div(class = "column",
+                div(class = "header",
+                  "Select vessel type"
+                ),
+                dropdown_input(
+                  input_id = ns("type"),
+                  default_text = "Vessel Type",
+                  choices = unique(vessels::ships2$ship_type),
+                  value = "Cargo"
+                )
+            ),
+            div(class = "column",
+              div(class = "header",
+                "Select vessel name"
+              ),
+              dropdown_input(
+                input_id = ns("ship"),
+                default_text = "Vessel name, choose a type first",
+                choices = NULL
+              )
+            )
+        )
+      
+      
+    )
+
+  )
+}
+    
+#' dropdowns Server Function
+#' 
+#' @import data.table
+#'
+#' @noRd 
+mod_dropdowns_server <- function(id, r){
+  
+  moduleServer(
+    id = id,
+    function(input, output, session) {
+      ns <- session$ns
+      
+      observeEvent(input$type, {
+        update_dropdown_input(
+          session = session,
+          input_id = "ship",
+          choices = unique(vessels::ships2$SHIPNAME[ships2$ship_type == input$type]),
+          value = "PINTA"
+        )
+      })
+      
+      observeEvent(input$ship, {
+        r$ship <- input$ship
+      })
+      
+    }
+  )
+ 
+}
+
+
