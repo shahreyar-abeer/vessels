8a6835abff2b0cc1eafd1085423fd79bf5f3eb8e
shahreyar-abeer <shahreyar.abeer@gmail.com>
2020-10-12 22:00:35 +0600

    script to add data

--- data-raw/ships.R
+++ data-raw/ships.R
@@ -3,8 +3,11 @@
 
 ships <- data.table::fread("../../Documents/ships_data/ships.csv")
 
-to_keep <- c("LAT", "LON", "SHIPNAME", "SHIPTYPE", "ELAPSED", "SPEED", "DATETIME")
+ships <- ships[, DATETIME := as.POSIXct(DATETIME)]
 
-ships2 <- ships[, ..to_keep]
+to_keep <- c("LAT", "LON", "SHIPNAME", "ship_type", "ELAPSED", "SPEED", "DATETIME",
+             "LENGTH", "FLAG", "WIDTH")
+
+ships2 <- ships[, ..to_keep][order(ship_type, SHIPNAME, DATETIME)]
 
 usethis::use_data(ships2, overwrite = TRUE)
