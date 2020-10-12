8b6c00757d932e381bba02e510b91a3f78509da2
shahreyar-abeer <shahreyar.abeer@gmail.com>
2020-10-12 18:53:46 +0600

    used divs from semantic ui

--- R/app_ui.R
+++ R/app_ui.R
@@ -3,14 +3,66 @@
 #' @param request Internal parameter for `{shiny}`. 
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @import shiny.semantic
+#' @importFrom reactable reactableOutput
 #' @noRd
 app_ui <- function(request) {
   tagList(
-    # Leave this function for adding external resources
     golem_add_external_resources(),
-    # List the first level UI elements here 
-    fluidPage(
-      h1("vessels")
+    semanticPage(
+      title = "Vessels",
+      
+      div(class = "ui container", style = "width:1127px;",
+          
+          h1(class = "ui header", style = "color:#aaa",
+            div(class = "content",
+              "Vessels: An entry point"
+            ),
+            div(class = "content", style = "float:right;",
+              tags$img(src = "https://raw.githubusercontent.com/rstudio/hex-stickers/4a66b3c2f37423aa2139ecd2123b3066895a8c98/SVG/shiny.svg",
+                       width = "60px")
+            )
+          ),
+          br(),
+          div(class = "ui container",
+              mod_dropdowns_ui("drops"),
+          ),
+          div(class = "ui raised segment", style = "width: 1127px;",
+            mod_map_ui("map"),
+            br(),
+            div(class = "ui grid",
+              div(class = "four column row",
+                div(class = "column", style = "width:40% !important",
+                    card(style = "width: 100%",
+                         div(class = "content",
+                             div(class = "header", 
+                                 textOutput("vessel")
+                             ),
+                             div(class = "meta", "Longest run"), 
+                             div(class = "description", textOutput("note"))
+                         )
+                    )
+                ),
+                div(class = "column", style = "width:60% !important",
+                    card(style = "width: 100%",
+                      div(class = "content",
+                        div(class = "header", 
+                          "Vessel details"
+                        ),
+                        div(class = "description", reactableOutput("details"))
+                    )
+                  )
+                )
+              )
+            )
+            
+          )
+          
+          
+          
+      )
+      
+      
     )
   )
 }
